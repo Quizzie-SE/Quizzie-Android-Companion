9e1a623f6a4baeebe5463a08c6e2f134d64a311f
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-03-16 06:42:19 +0530

    Initial commit

---
+++ app/src/main/java/com/quizzie/quizzieapp/ui/splash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.quizzie.quizzieapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.quizzie.quizzieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
