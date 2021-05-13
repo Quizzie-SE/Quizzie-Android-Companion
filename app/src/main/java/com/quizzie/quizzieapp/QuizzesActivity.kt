0ba3e9bf95ecb4964d6376ca4c8310705082cc41
Dinesh B S <dineshbs444@gmail.com>
2021-05-13 16:38:19 +0530

    Added list item for Quizzes Rv

--- app/src/main/java/com/quizzie/quizzieapp/QuizzesActivity.kt
+++ app/src/main/java/com/quizzie/quizzieapp/QuizzesActivity.kt
@@ -1,17 +1,37 @@
 package com.quizzie.quizzieapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
-import android.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
+
 
 class QuizzesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizzes)
 
-        val toolbar: MaterialToolbar = findViewById<Toolbar>(R.id.toolbar) as MaterialToolbar
-        setSupportActionBar(toolbar)
+        /*val toolbar: MaterialToolbar = findViewById<Toolbar>(R.id.toolbar) as MaterialToolbar
+        setSupportActionBar(toolbar)*/
+        val actionBar: ActionBar? = supportActionBar
+        val colorDrawable = ColorDrawable(Color.parseColor("#007BBA"))
+        actionBar!!.setBackgroundDrawable(colorDrawable)
+        supportActionBar!!.title = "Quizzes"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.itemId
+        if (id == R.id.mybutton) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
