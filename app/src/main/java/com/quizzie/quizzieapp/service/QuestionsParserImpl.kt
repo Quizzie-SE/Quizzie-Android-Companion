9b17fe3daf54988349ff29735eed83286bd4cb57
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-06-02 13:04:18 +0530

    fix(scan): Unwanted newlines

--- app/src/main/java/com/quizzie/quizzieapp/service/QuestionsParserImpl.kt
+++ app/src/main/java/com/quizzie/quizzieapp/service/QuestionsParserImpl.kt
@@ -1,25 +1,41 @@
 package com.quizzie.quizzieapp.service
 
 import com.google.mlkit.vision.text.Text
-import com.quizzie.quizzieapp.model.domain.Question
 import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class QuestionsParserImpl @Inject constructor(
     externalScope: CoroutineScope
-): QuestionsParser(externalScope) {
+) : QuestionsParser(externalScope) {
+
+    companion object {
+        private const val STOP_SYMBOLS = ":.!?"
+    }
 
     private fun isUnwanted(line: String) = line.trim().startsWith("question", true)
     private fun isOption(line: String) = line.matches(Regex("""\(?\w[.)]\s.*"""))
-    private fun getOptionChar(line: String) = """(?<=\(?)\w(?=[.)]\s)""".toRegex().find(line, 0)?.value
+    private fun getOptionChar(line: String) =
+        """(?<=\(?)\w(?=[.)]\s)""".toRegex().find(line, 0)?.value
+
     private fun isCorrectUpcomingOption(line: String, state: MCQStates.Options): Boolean {
-        return isOption(line) && state.optionType.match(state.optionNo + 1, getOptionChar(line) ?: "")
+        return isOption(line) && state.optionType.match(
+            state.optionNo + 1,
+            getOptionChar(line) ?: ""
+        )
     }
+
     private fun isCorrectOption(line: String, state: MCQStates.Options): Boolean {
         return isOption(line) && state.optionType.match(state.optionNo, getOptionChar(line) ?: "")
     }
+
     private fun hasQnoPrefix(line: String) = line.matches("""\(?\d+([.)])\s.*""".toRegex())
-    private fun removeQnoPrefix(line: String) = line.replaceFirst("""\(?\d+([.)])\s""".toRegex(), "")
+    private fun removeQnoPrefix(line: String) =
+        line.replaceFirst("""\(?\d+([.)])\s""".toRegex(), "")
+
+    private fun manageNewLine(line: String, blankLinesBefore: Int, question: String) =
+        (if (STOP_SYMBOLS.contains(question.last(), true)
+        ) "\n".repeat(blankLinesBefore + 1) else " ") + line
+
 
     override fun parse(line: Text.Line, state: MCQStates, blankLinesBefore: Int): Action {
         val lineText = line.text
@@ -28,7 +44,8 @@ class QuestionsParserImpl @Inject constructor(
             is MCQStates.Options -> {
 
                 if (isOption(lineText) && state.lineNo == 0 && state.optionNo == 0) {
-                    state.optionType = OptionType.getOptionType(getOptionChar(lineText) ?: "") ?: OptionType.DECIMAL
+                    state.optionType = OptionType.getOptionType(getOptionChar(lineText) ?: "")
+                        ?: OptionType.DECIMAL
                 }
 
                 if (isCorrectOption(lineText, state) || state.lineNo > 0) {
@@ -37,11 +54,15 @@ class QuestionsParserImpl @Inject constructor(
                     }
                     if (state.optionNo >= 4) return Action.EXIT
 
-                    if (state.lineNo > 0) options[state.optionNo] += "\n".repeat(blankLinesBefore + 1)
-
-                    options[state.optionNo] += if (isCorrectOption(lineText, state))
+                    val processedLine = if (isCorrectOption(lineText, state))
                         lineText.removePrefix(line.elements[0].text).trim() else lineText
                     state.nextLine()
+
+                    options[state.optionNo] += if (state.lineNo > 0) processedLine else manageNewLine(
+                        processedLine,
+                        blankLinesBefore,
+                        question
+                    )
 
                     return Action.LINE
                 }
@@ -50,20 +71,25 @@ class QuestionsParserImpl @Inject constructor(
 
             is MCQStates.Question -> {
                 if (!isOption(lineText) || question.isBlank()) {
-                    question += if (state.lineNo == 0 && hasQnoPrefix(lineText)) {
+                    val processedLine = if (state.lineNo == 0 && hasQnoPrefix(lineText)) {
                         removeQnoPrefix(lineText)
                     } else lineText
 
-                    question += "\n".repeat(blankLinesBefore)
-                    state.nextLine()
+                    question += if (question.isBlank()) processedLine else manageNewLine(
+                        processedLine,
+                        blankLinesBefore,
+                        question
+                    )
 
-                    if (isOption(lineText)) return Action.LINE_STATE
+                    state.nextLine()
+                    return Action.LINE
                 }
-                return if(isOption(lineText)) Action.STATE else Action.LINE
+
+                return Action.STATE
             }
 
             MCQStates.UnwantedText -> {
-                return if(isUnwanted(lineText)) Action.LINE_STATE else Action.STATE
+                return if (isUnwanted(lineText)) Action.LINE_STATE else Action.STATE
             }
         }
 
