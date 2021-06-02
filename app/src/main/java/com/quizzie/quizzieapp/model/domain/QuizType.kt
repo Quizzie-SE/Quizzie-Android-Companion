f6fbd0644c91dd250e0b47f79dcb6fa5bfb7d189
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-06-02 12:58:52 +0530

    feat(edit-quiz): Add quiz type slider

--- app/src/main/java/com/quizzie/quizzieapp/model/domain/QuizType.kt
+++ app/src/main/java/com/quizzie/quizzieapp/model/domain/QuizType.kt
@@ -1,6 +1,8 @@
 package com.quizzie.quizzieapp.model.domain
 
+import com.google.gson.annotations.SerializedName
+
 enum class QuizType {
-    Public,
-    Private
+    @SerializedName(value = "Public", alternate = ["public"]) Public,
+    @SerializedName("Private", alternate = ["private"]) Private
 }
